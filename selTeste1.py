c15be8f8ef32529c43fa71c7eea6c96a113d3725
LuizDesafio1 <fesfesfse6@gmail.com>
2025-03-24 16:21:32 -0300

    Add files via upload

---
+++ selTeste1.py
@@ -0,0 +1,60 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+def get_default_edge_options():
+    options = webdriver.EdgeOptions()
+    options.add_argument("--no-sandbox")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    return options
+
+options = get_default_edge_options()
+driver = webdriver.Edge(options=options)  
+
+
+driver.get("http://localhost:8501/manager")  
+time.sleep(25)
+#teste 1- Teste de carregamento de períodos disponíveis
+
+aba_produtividade = driver.find_element(By.ID, 'tabs-bui2-tab-4')
+aba_produtividade.click()
+time.sleep(20)
+
+
+ano_inicial = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[1]/div/div/div/div[1]/div/div/div/div[2]')
+ano_inicial.click()
+time.sleep(4)
+ano_inicialA = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[2]/input')
+ano_inicialA.send_keys(Keys.ENTER)
+time.sleep(4)
+
+
+mes_inicialA = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[1]/div/div/div/div[2]/div/div/div/div[2]')
+mes_inicialA.click()
+time.sleep(4)
+mes_inicial = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[1]/div/div/div/div[2]/div/div/div/div[1]/div[2]/input')
+mes_inicial.send_keys(Keys.ENTER)
+time.sleep(2)
+
+
+ano_finalA = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[2]')
+ano_finalA.click()
+time.sleep(4)
+ano_final = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/input')
+ano_final.send_keys(Keys.ENTER)
+time.sleep(2)
+
+
+
+
+mes_finalA = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div/div/div[2]')
+mes_finalA.click()
+time.sleep(4)
+mes_final = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div/div/div[1]/div[2]/input')
+mes_final.send_keys(Keys.ENTER)
+time.sleep(2)
+
+time.sleep(10)
+driver.quit()
---
+++ selTeste2.py
@@ -0,0 +1,50 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+def get_default_edge_options():
+    options = webdriver.EdgeOptions()
+    options.add_argument("--no-sandbox")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    return options
+
+options = get_default_edge_options()
+driver = webdriver.Edge(options=options)  
+
+
+driver.get("http://localhost:8501/manager")  
+time.sleep(25)
+#teste2-Teste de seleção de período
+
+aba_produtividade = driver.find_element(By.ID, 'tabs-bui2-tab-4')
+aba_produtividade.click()
+time.sleep(5)
+
+
+ano_inicial = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[2]/input')
+ano_inicial.send_keys("2025")  
+ano_inicial.send_keys(Keys.ENTER)
+time.sleep(2)
+
+mes_inicial = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[1]/div/div/div/div[2]/div/div/div/div[1]/div[2]/input')
+mes_inicial.send_keys("3")
+mes_inicial.send_keys(Keys.ENTER)
+time.sleep(2)
+
+ano_final = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/input')
+ano_final.send_keys("2025")  
+ano_final.send_keys(Keys.ENTER)
+time.sleep(2)
+
+mes_final = driver.find_element(By.XPATH, '//*[@id="tabs-bui2-tabpanel-4"]/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div/div/div[1]/div[2]/input')
+mes_final.send_keys("4")
+mes_final.send_keys(Keys.ENTER)
+time.sleep(2)
+
+
+
+
+time.sleep(10)
+driver.quit()
