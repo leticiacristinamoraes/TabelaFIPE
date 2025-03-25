85665fe84ea1cf19217c66795b3442864893c982
Vaniza <15125967+Vaniza@users.noreply.github.com>
2025-03-24 22:14:34 -0300

    Update research_stats.py

--- app/database/research_stats.py
+++ app/database/research_stats.py
@@ -3,11 +3,32 @@ import pandas as pd
 from app.database.config import get_connection
 from calendar import monthrange
 
-def get_research_data(pesquisador_id, ano_inicio, mes_inicio, ano_fim, mes_fim):
+def create_research_stats_table():
     conn = get_connection()
     cur = conn.cursor()
 
-    
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS research_stats (
+            id SERIAL PRIMARY KEY,
+            researcher_id INTEGER NOT NULL,
+            search_date DATE NOT NULL,
+            search_count INTEGER NOT NULL,
+            FOREIGN KEY (researcher_id) REFERENCES users(id) ON DELETE SET NULL
+        );
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def get_research_data(pesquisador_id, ano_inicio, mes_inicio, ano_fim, mes_fim):
+
+    create_research_stats_table()
+
+    conn = get_connection()
+    cur = conn.cursor()
+
     _, last_day = monthrange(ano_fim, mes_fim)
 
     cur.execute("""
@@ -22,4 +43,3 @@ def get_research_data(pesquisador_id, ano_inicio, mes_inicio, ano_fim, mes_fim):
     conn.close()
 
     return pd.DataFrame(data, columns=["search_date", "search_count"])
-
